8983d28310fb0016d38b62ff9feb9981c265358a
marcos <marcoscorreats@gmail.com>
2023-05-11 02:28:31 -0300

    feat: enabling core

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT);
   Logger.log(`Server running on port ${process.env.PORT}`, 'Bootstrap');
 }
